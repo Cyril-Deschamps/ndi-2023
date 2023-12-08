bb9c8ec4f23d4fc6b5af6b6ea85a83b83c9751b0
mathias <mathias.puyfages@etu.umontpellier.fr>
2023-12-08 02:06:46 +0100

    feat: ajout du snake et des images

--- src/pages/snake.tsx
+++ src/pages/snake.tsx
@@ -1,8 +1,132 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react"
+import AppleLogo from "../assets/img/icons/applePixels.png"
+import Monitor from "../assets/img/icons/oldMonitor.png"
+import useInterval from "../services/snake/useInterval"
+import Image from "next/image"
+import dynamic from "next/dynamic"
 
+const canvasX = 1000
+const canvasY = 1000
+const initialSnake = [ [ 4, 10 ], [ 4, 10 ] ]
+const initialApple = [ 14, 10 ]
+const scale = 50
+const timeDelay = 100
 
-function Snake(){
-    return (<div><h1>Feur</h1></div>)
+function Snake() {
+	const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	const [ snake, setSnake ] = useState(initialSnake)
+	const [ apple, setApple ] = useState(initialApple)
+	const [ direction, setDirection ] = useState([ 0, -1 ])
+	const [ delay, setDelay ] = useState<number | null>(null)
+	const [ gameOver, setGameOver ] = useState(false)
+	const [ score, setScore ] = useState(0)
+    const [showPopup, setShowPopup] = useState<boolean>(false);
+
+	useInterval(() => runGame(), delay)
+
+	useEffect(
+		() => {
+			let fruit = document.getElementById("fruit") as HTMLCanvasElement
+			if (canvasRef.current) {
+				const canvas = canvasRef.current
+				const ctx = canvas.getContext("2d")
+				if (ctx) {
+					ctx.setTransform(scale, 0, 0, scale, 0, 0)
+					ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+					ctx.fillStyle = "#a3d001"
+					snake.forEach(([ x, y ]) => ctx.fillRect(x, y, 1, 1))
+					ctx.drawImage(fruit, apple[0], apple[1], 1, 1)
+				}
+			}
+		},
+		[ snake, apple, gameOver ]
+	)
+
+	function handleSetScore() {
+		if (score > Number(localStorage.getItem("snakeScore"))) {
+			localStorage.setItem("snakeScore", JSON.stringify(score))
+		}
+	}
+
+	function play() {
+		setSnake(initialSnake)
+		setApple(initialApple)
+		setDirection([ 1, 0 ])
+		setDelay(timeDelay)
+		setScore(0)
+		setGameOver(false)
+	}
+
+	function checkCollision(head: number[]) {
+		for (let i = 0; i < head.length; i++) {
+			if (head[i] < 0 || head[i] * scale >= canvasX) return true
+		}
+		for (const s of snake) {
+			if (head[0] === s[0] && head[1] === s[1]) return true
+		}
+		return false
+	}
+
+	function appleAte(newSnake: number[][]) {
+		let coord = apple.map(() => Math.floor(Math.random() * canvasX / scale))
+		if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+			let newApple = coord
+			setScore(score + 1)
+			setApple(newApple)
+			return true
+		}
+		return false
+	}
+
+	function runGame() {
+		const newSnake = [ ...snake ]
+		const newSnakeHead = [ newSnake[0][0] + direction[0], newSnake[0][1] + direction[1] ]
+		newSnake.unshift(newSnakeHead)
+		if (checkCollision(newSnakeHead)) {
+			setDelay(null)
+			setGameOver(true)
+			handleSetScore()
+		}
+		if (!appleAte(newSnake)) {
+			newSnake.pop()
+		}
+		setSnake(newSnake)
+	}
+
+	function changeDirection(e: React.KeyboardEvent<HTMLDivElement>) {
+		switch (e.key) {
+			case "ArrowLeft":
+				setDirection([ -1, 0 ])
+				break
+			case "ArrowUp":
+				setDirection([ 0, -1 ])
+				break
+			case "ArrowRight":
+				setDirection([ 1, 0 ])
+				break
+			case "ArrowDown":
+				setDirection([ 0, 1 ])
+				break
+		}
+	}
+      
+
+    if (typeof localStorage === "undefined") return <></>
+
+	return (
+		<div onKeyDown={(e) => changeDirection(e)}>
+			<Image id="fruit" src={AppleLogo} alt="fruit" width="30" />
+			<canvas className="w-585 h-440 fixed top-43 left-1/2 transform translate-x-[-50%] translate-y-[-50%]" ref={canvasRef} width={`${canvasX}px`} height={`${canvasY}px`} />
+			{gameOver && <div className="fixed top-43.2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl">Game Over</div>}
+			<button onClick={play}  className="fixed top-80 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white bg-purple-800 border-2 border-white p-10 text-sm shadow-md font-bold tracking-wider uppercase font-cursive">
+				Play
+			</button>
+			<div className="float-right m-30 mt-50 shadow-[0px_4px_13px_0px_rgba(48,26,74,0.63)]" >
+				<h2>Score: {score}</h2>
+				<h2>High Score: {localStorage.getItem("snakeScore")}</h2>
+			</div>
+		</div>
+	)
 }
 
-export default Snake
+export default dynamic(() => Promise.resolve(Snake))
