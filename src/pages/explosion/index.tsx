656c8844971d24541e9ccea939fa5cb11b0eee52
KyZiiKs <86744162+Titanguy4@users.noreply.github.com>
2023-12-08 03:47:36 +0100

    commit

--- src/pages/explosion/index.tsx
+++ src/pages/explosion/index.tsx
@@ -63,20 +63,23 @@ const GenerateItem = ({
   Ennemie: StaticImageData[];
   Alie: StaticImageData[];
 }): GeneratedItem => {
-  const tableauCombine: StaticImageData[] = [...Ennemie, ...Alie];
-  const rand = Math.floor(Math.random() * tableauCombine.length);
-  const etat = rand < Ennemie.length ? "Ennemie" : "Alie";
+  const ennemieOrAlie = Math.random() < 0.5 ? "Ennemie" : "Alie";
+  const sourceArray = ennemieOrAlie === "Ennemie" ? Ennemie : Alie;
+
+  const rand = Math.floor(Math.random() * sourceArray.length);
+  const etat = ennemieOrAlie;
 
   const position = {
     top: Math.round(75 + Math.random() * (window.innerHeight * 0.6 - 75)),
-    left: Math.round(75 + Math.random() * (window.innerWidth - 75)),
+    left: Math.round(75 + Math.random() * (window.innerWidth - 175)),
   };
 
-  return { image: tableauCombine[rand], etat, clicked: false, position };
+  return { image: sourceArray[rand], etat, clicked: false, position };
 };
 
 const Home: React.FC = () => {
   const [items, setItems] = useState<GeneratedItem[]>([]);
+  const [score, setScore] = useState<number>(0);
 
   useEffect(() => {
     const generatedItems = Array.from({ length: 10 }, () =>
@@ -88,22 +91,46 @@ const Home: React.FC = () => {
   const setOnClick = (index: number) => {
     setItems((prevItems) => {
       const newItems = [...prevItems];
-      newItems[index].clicked = true;
+      const clickedItem = newItems[index];
+
+      if (!clickedItem.clicked) {
+        // Vérifiez si l'élément n'a pas déjà été cliqué
+        if (clickedItem.etat === "Ennemie") {
+          setScore((prevScore) => prevScore + 1);
+        } else if (clickedItem.etat === "Alie") {
+          setScore((prevScore) => prevScore - 1);
+        }
+
+        clickedItem.clicked = true;
+
+        // Vérifiez si l'image est "boom" et affichez-la pendant 2 secondes
+        if (clickedItem.image === boom) {
+          setTimeout(() => {
+            // Générer une nouvelle image après 2 secondes
+            newItems[index] = GenerateItem({ Ennemie, Alie });
+            setItems(newItems);
+          }, 2000);
+        }
+      }
+
       return newItems;
     });
   };
 
   return (
-    <div className={"items"}>
-      {items.map((item, index) => (
-        <Item
-          key={index}
-          clicked={item.clicked}
-          image={item.image}
-          onClick={() => setOnClick(index)}
-          position={item.position}
-        />
-      ))}
+    <div>
+      <div className={"score"}>Score: {score}</div>
+      <div className={"items"}>
+        {items.map((item, index) => (
+          <Item
+            key={index}
+            clicked={item.clicked}
+            image={item.image}
+            onClick={() => setOnClick(index)}
+            position={item.position}
+          />
+        ))}
+      </div>
     </div>
   );
 };
